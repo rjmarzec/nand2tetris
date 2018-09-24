5a429aa02c5a1f8bfeae66cf605d254376b6be5f
rjmarzec <robertmarzecj@gmail.com>
2018-09-24 14:49:23 -0500

    changed variable and method names to follow python naming conventions

--- main.py
+++ main.py
@@ -1,121 +1,131 @@
 #########################################
-########## Main Running Area ############
+# Main Running Area #####################
 #########################################
 
-def vmToASM():
-	file = open("test.vm", "r")
-	rawFileLineList = getFileLinesAsList(file)
-	cleanedVMLineList = cleanLineList(rawFileLineList)
-	hackLineList = convertVMToHack(cleanedVMLineList)
-	writeHackToFile(hackLineList)
+
+def vm_to_asm():
+	input_file = open("test.vm", "r")
+	raw_file_line_list = get_file_lines_as_list(input_file)
+	cleaned_vm_line_list = clean_line_list(raw_file_line_list)
+	hack_line_list = convert_vm_to_hack(cleaned_vm_line_list)
+	write_hack_to_file(hack_line_list)
+
 
 #########################################
-############ Function Area ##############
+# Function Area #########################
 #########################################
 
-def getFileLinesAsList(inputFile):
-	return inputFile.readlines()
 
-def cleanLineList(inputLineList):
-	resultLineList = []
-	for line in inputLineList:
+def get_file_lines_as_list(input_file):
+	return input_file.readlines()
+
+
+def clean_line_list(input_line_list):
+	result_line_list = []
+	for line in input_line_list:
 		# removes any spaces and instances of spaces, \n, \r, and \t
-		tempLine = line
-		tempLine = tempLine.replace("\n", "")
-		tempLine = tempLine.replace("\r", "")
-		tempLine = tempLine.replace("\t", "")
+		temp_line = line
+		temp_line = temp_line.replace("\n", "")
+		temp_line = temp_line.replace("\r", "")
+		temp_line = temp_line.replace("\t", "")
 
 		# removes any comments from a line if they exist
-		if "//" in tempLine:
-			tempLine = tempLine[:tempLine.index("//")]
+		if "//" in temp_line:
+			temp_line = temp_line[:temp_line.index("//")]
 
 		# clears any starting and ending spaces
-		tempLine.strip()
+		temp_line.strip()
 
 		# turns any spaces longer than 1 space into 1 space
-		while "  " in tempLine:
-			tempLine.replace("  ", " ")
+		while "  " in temp_line:
+			temp_line.replace("  ", " ")
 
 		# ignores the line if there is no actual code on it
-		if tempLine != "":
-			resultLineList.append(tempLine)
-	return resultLineList
+		if temp_line != "":
+			result_line_list.append(temp_line)
+	return result_line_list
 
-def convertVMToHack(inputLineList):
-	resultLineList = []
 
-	for line in inputLineList:
-		resultLine = ""
-		lineCommandType = getCommandType(line)
-		resultLine = convertLineToHack(line, lineCommandType)
-		resultLineList.append(resultLine)
+def convert_vm_to_hack(input_line_list):
+	result_line_list = []
 
-	return resultLineList
+	for line in input_line_list:
+		line_command_type = get_command_type(line)
+		result_line = convert_line_to_hack(line, line_command_type)
+		result_line_list.append(result_line)
 
-def getCommandType(inputLine):
+	return result_line_list
+
+
+def get_command_type(input_line):
 	if "add" or "sub":
 		# add more artimethic commands to the if statement
 		return "C_ARITHMETIC"
-	elif "push" in inputLine:
+	elif "push" in input_line:
 		return "C_POP"
-	elif "pop" in inputLine:
+	elif "pop" in input_line:
 		return "C_PUSH"
-	elif "label" in inputLine:
+	elif "label" in input_line:
 		return "C_GOTO"
-	elif "if" in inputLine:
+	elif "if" in input_line:
 		return "C_IF"
-	elif "function" in inputLine:
+	elif "function" in input_line:
 		return "C_FUNCTION"
-	elif "return" in inputLine:
+	elif "return" in input_line:
 		return "C_RETURN"
 	else:
 		# elif "call" in line:
 		return "C_CALL"
 
-def convertLineToHack(inputLine, commandType):
-	if commandType == "C_ARITHMETIC":
-		return writeArithmetic(inputLine, commandType)
-	elif commandType == "C_PUSH" or commandType == "C_POP":
-		return writePushPop(inputLine, commandType)
-	elif commandType == "C_LABEL":
+
+def convert_line_to_hack(input_line, command_type):
+	if command_type == "C_ARITHMETIC":
+		return write_arithmetic(input_line, command_type)
+	elif command_type == "C_PUSH" or command_type == "C_POP":
+		return write_push_pop(input_line, command_type)
+	elif command_type == "C_LABEL":
 		return
-	elif commandType == "C_GOTO":
+	elif command_type == "C_GOTO":
 		return
-	elif commandType == "C_IF":
+	elif command_type == "C_IF":
 		return
-	elif commandType == "C_FUNCTION":
+	elif command_type == "C_FUNCTION":
 		return
-	elif commandType == "C_RETURN":
+	elif command_type == "C_RETURN":
 		return
-	elif commandType == "C_CALL":
+	elif command_type == "C_CALL":
 		return
 	else:
 		return "ERROR: Command not specified"
 
-def writeArithmetic(inputLine, commandType):
-	resultString = ""
-	if "add" in inputLine:
-		resultString += "tempAddText"
-	elif "sub" in inputLine:
-		resultString += "tempSubText"
-	return resultString
 
-def writePushPop(inputLine, commandType):
-	resultString = ""
-	editedString = ""
-	if commandType == "C_PUSH":
+def write_arithmetic(input_line, command_type):
+	result_string = ""
+	if "add" in input_line:
+		result_string += "tempAddText"
+	elif "sub" in input_line:
+		result_string += "tempSubText"
+	return result_string
+
+
+def write_push_pop(input_line, command_type):
+	result_string = ""
+	edited_string = ""
+	if command_type == "C_PUSH":
 		# editedString =
 		# resultString
 		return
-	elif commandType == "C_POP":
+	elif command_type == "C_POP":
 		return
-	return resultString
+	return result_string
+
 
 #########################################
-############ Testing Area ###############
+# Testing Area ##########################
 #########################################
 
-def writeHackToFile(inputLineList):
+
+def write_hack_to_file(inputLineList):
 	file = open("test.asm", "w")
 	writeString = ""
 	for line in inputLineList:
@@ -124,4 +134,5 @@ def writeHackToFile(inputLineList):
 	file.write(writeString)
 	file.close()
 
-vmToASM()
+
+vm_to_asm()
