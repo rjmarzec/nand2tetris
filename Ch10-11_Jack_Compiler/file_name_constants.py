019fcb50972d4d9fc829f6d572450cd465e14108
rjmarzec <robertmarzecj@gmail.com>
2019-02-25 15:15:48 -0600

    Commited the file for I/O name references

---
+++ Ch10-11_Jack_Compiler/file_name_constants.py
@@ -0,0 +1,55 @@
+# A file containing constants of the project file names so that it is easier to switch between them when testing
+
+#########################################
+# Template ##############################
+#########################################
+
+# Files Name ####################
+# ..._IN = ['TestFiles/Chapter.../.../....jack', '...']	(Input File Directory as a List)
+# ..._OUT = '.../.../....vm'	(Output File Directory as a List)
+
+#########################################
+# Chapter 9 Files #######################
+#########################################
+
+# ArrayTest
+ARRAY_TEST_IN = \
+	['TestFiles/Chapter9/ArrayTest/Main.jack']
+ARRAY_TEST_OUT = \
+	['TestFiles/Chapter9/ArrayTestOutput/Main.xml']
+ARRAY_TEST_OUT_T = \
+	['TestFiles/Chapter9/ArrayTestOutput/MainT.xml']
+
+# ExpressionLessSquare
+EXPRESSION_LESS_SQUARE_IN = \
+	['TestFiles/Chapter9/ExpressionLessSquare/Main.jack',
+		'TestFiles/Chapter9/ExpressionLessSquare/Square.jack',
+		'TestFiles/Chapter9/ExpressionLessSquare/SquareGame.jack']
+EXPRESSION_LESS_SQUARE_OUT = \
+	['TestFiles/Chapter9/ExpressionLessSquareOutput/Main.xml',
+		'TestFiles/Chapter9/ExpressionLessSquareOutput/Square.xml',
+		'TestFiles/Chapter9/ExpressionLessSquareOutput/SquareGame.xml']
+EXPRESSION_LESS_SQUARE_OUT_T = \
+	['TestFiles/Chapter9/ExpressionLessSquareOutput/MainT.xml',
+		'TestFiles/Chapter9/ExpressionLessSquareOutput/SquareT.xml',
+		'TestFiles/Chapter9/ExpressionLessSquareOutput/SquareGameT.xml']
+
+# Square
+SQUARE_IN = \
+	['TestFiles/Chapter9/Square/Main.jack',
+		'TestFiles/Chapter9/Square/Square.jack',
+		'TestFiles/Chapter9/Square/SquareGame.jack']
+SQUARE_OUT = \
+	['TestFiles/Chapter9/SquareOutput/Main.xml',
+		'TestFiles/Chapter9/SquareOutput/Square.xml',
+		'TestFiles/Chapter9/SquareOutput/SquareGame.xml']
+SQUARE_OUT_T = \
+	['TestFiles/Chapter9/SquareOutput/MainT.xml',
+		'TestFiles/Chapter9/SquareOutput/SquareT.xml',
+		'TestFiles/Chapter9/SquareOutput/SquareGameT.xml']
+
+#########################################
+# Chapter 10 Files ######################
+#########################################
+
+# ...
