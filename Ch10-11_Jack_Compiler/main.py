fc213aacb9eb3c04c365886dc39ee220251bb023
rjmarzec <robertmarzecj@gmail.com>
2019-03-04 15:12:53 -0600

    Added compilation method framework

--- Ch10-11_Jack_Compiler/main.py
+++ Ch10-11_Jack_Compiler/main.py
@@ -14,16 +14,74 @@ output_t_file_name_list = file_name_constants.ARRAY_TEST_OUT_T
 # Cleanup and Start Functions ###########
 #########################################
 
-def get_file_lines_as_list(file_path):
+def get_file_lines_as_string(file_path):
 	input_file = open(file_path, "r")
 	result_list = input_file.readlines()
 	input_file.close()
 
-	return result_list
+	result_string = ""
+	for line in result_list:
+		result_string += line
+
+	return result_string
 
 
 def other_function(variable_input):
 	print('temp"')
+	return
+
+
+#########################################
+# Element Compiler ######################
+#########################################
+
+def compile_statement():
+	# if_statement | while_statement | let statement
+	return
+
+
+def compile_statements():
+	# statement*
+	return
+
+
+def compile_if_statement():
+	# 'if' '(' expression ')' '{' statements '}'
+	return
+
+
+def compile_while_statement():
+	# 'if' '(' expression ')' '{' statements '}'
+	return
+
+
+def compile_let_statement():
+	# 'let' var_name '=' expressions ';'
+	return
+
+
+def compile_expression():
+	# term (op term)?
+	return
+
+
+def compile_term():
+	# var_name | constant
+	return
+
+
+def compile_var_name():
+	# a string not beginning with a digit
+	return
+
+
+def compile_constant():
+	# a decimal number
+	return
+
+
+def compile_op():
+	# '+' | '-' | '=' | '>' | '<'
 	return
 
 
