bfe6e63ef125181da32b5be7b491a650a6f6b191
rjmarzec <robertmarzecj@gmail.com>
2019-03-05 15:14:49 -0600

    Started progressing on the tokenizer

--- Ch10-11_Jack_Compiler/main.py
+++ Ch10-11_Jack_Compiler/main.py
@@ -1,13 +1,20 @@
 import file_name_constants
 
 #########################################
-# Global Variables ######################
+# Global Variables & Constants ##########
 #########################################
 
 # Lists of the I/O file paths. The constant name needs to be changed to adjust which file is compiled.
-input_file_name_list = file_name_constants.ARRAY_TEST_IN
-output_file_name_list = file_name_constants.ARRAY_TEST_OUT
-output_t_file_name_list = file_name_constants.ARRAY_TEST_OUT_T
+input_file_path_list = file_name_constants.ARRAY_TEST_IN
+output_file_path_list = file_name_constants.ARRAY_TEST_OUT
+output_t_file_path_list = file_name_constants.ARRAY_TEST_OUT_T
+
+
+# Tokenizer Classifications
+keyword_tokens = \
+	['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true',
+		'if', 'else', 'while', 'return']
+symbol_tokens = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
 
 
 #########################################
@@ -26,8 +33,27 @@ def get_file_lines_as_string(file_path):
 	return result_string
 
 
-def other_function(variable_input):
-	print('temp"')
+#########################################
+# Tokenizer Functions ###################
+#########################################
+
+def tokenize(input_file_string, output_file_path):
+	current_index = 0
+	ending_index = len(input_file_string)
+	token_list = []
+
+	while current_index < ending_index:
+		if input_file_string[current_index, current_index + 1] == ' ' or \
+				input_file_string[current_index, current_index + 1] == '\n':
+			current_index += 1
+		else:
+			for keyword in keyword_tokens:
+				if input_file_string[current_index, current_index + len(keyword)] == keyword:
+					current_index += len(keyword)
+					token_list.append(['keyword', keyword])
+
+
+def write_tokens_to_xml(token_as_list, output_file_path):
 	return
 
 
@@ -89,4 +115,12 @@ def compile_op():
 # Testing Area ##########################
 #########################################
 
-print(file_name_constants.ARRAY_TEST_IN[0])
+print(file_name_constants.ARRAY_TEST_IN[0] + "\n")
+
+for file_path in input_file_path_list:
+	temp_string = get_file_lines_as_string(file_path)
+	print(temp_string)
+
+	# tokenize(temp_string)
+
+
