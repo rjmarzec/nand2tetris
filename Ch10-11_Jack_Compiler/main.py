bf0e5dd8c4f62623d389b7240b7fe7591755601d
rjmarzec <robertmarzecj@gmail.com>
2019-03-13 15:13:16 -0500

    fixed token keyword list order

--- Ch10-11_Jack_Compiler/main.py
+++ Ch10-11_Jack_Compiler/main.py
@@ -14,7 +14,7 @@ output_t_file_path_list = file_name_constants.SQUARE_OUT_T
 # Tokenizer Classifications
 keyword_tokens = \
 	['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true',
-		'if', 'else', 'while', 'return', 'do', 'let', 'this', 'null', 'true', 'false']
+		'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
 symbol_tokens = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
 integers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 spaces = [' ', '\n', '\t', '']
