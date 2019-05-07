da09de178c5e60d23cacd266221cfeafa849bbef
rjmarzec <robertmarzecj@gmail.com>
2019-05-07 14:56:21 -0500

    Added output files for the ch11 scripts and updated file references

--- Ch10-11_Jack_Compiler/file_name_constants.py
+++ Ch10-11_Jack_Compiler/file_name_constants.py
@@ -5,9 +5,10 @@
 #########################################
 
 # Files Name ####################
-# ..._in = ['TestFiles/Chapter.../.../....jack', '...']	(Input File Directory as a List)
-# ..._out = ['.../.../....vm', '...']	(Output File Directory as a List)
-# ..._out_t = ['.../.../....vm', '...']	(Tokenizer Output File Directory as a List)
+# ..._in = ['TestFiles/Chapter.../.../....jack', '...']	(Input File Directories)
+# ..._out_xml = ['.../.../....vm', '...']	(xml Output File Directories)
+# ..._out_t = ['.../.../....vm', '...']		(Tokenizer Output File Directories)
+# ..._out_vm = ['.../.../....vm', '...']	(Translated VM Output File Directories)
 # ... = [..._in, ..._out, ..._out_t] (A public collection of the above as a list, in the order shown above)
 
 #########################################
@@ -21,7 +22,9 @@ array_test_out_xml = \
 	['TestFiles/Chapter10/ArrayTestOutput/Main.xml']
 array_test_out_t = \
 	['TestFiles/Chapter10/ArrayTestOutput/MainT.xml']
-ARRAY_TEST = [array_test_in, array_test_out_xml, array_test_out_t]
+array_test_out_vm = \
+	['TestFiles/Chapter10/ArrayTestOutput/Main.vm']
+ARRAY_TEST = [array_test_in, array_test_out_xml, array_test_out_t, array_test_out_vm]
 
 # ExpressionLessSquare
 expression_less_square_in = \
@@ -36,22 +39,31 @@ expression_less_square_out_t = \
 	['TestFiles/Chapter10/ExpressionLessSquareOutput/MainT.xml',
 		'TestFiles/Chapter10/ExpressionLessSquareOutput/SquareT.xml',
 		'TestFiles/Chapter10/ExpressionLessSquareOutput/SquareGameT.xml']
-EXPRESSION_LESS_SQUARE = [expression_less_square_in, expression_less_square_out_xml, expression_less_square_out_t]
+expression_less_square_out_vm = \
+	['TestFiles/Chapter10/ExpressionLessSquareOutput/Main.vm',
+		'TestFiles/Chapter10/ExpressionLessSquareOutput/Square.vm',
+		'TestFiles/Chapter10/ExpressionLessSquareOutput/SquareGame.vm']
+EXPRESSION_LESS_SQUARE = [expression_less_square_in, expression_less_square_out_xml, expression_less_square_out_t, expression_less_square_out_vm]
 
 # Square
-square_in = \
+square_10_in = \
 	['TestFiles/Chapter10/Square/Main.jack',
 		'TestFiles/Chapter10/Square/Square.jack',
 		'TestFiles/Chapter10/Square/SquareGame.jack']
-square_out_xml = \
+square_10_out_xml = \
 	['TestFiles/Chapter10/SquareOutput/Main.xml',
 		'TestFiles/Chapter10/SquareOutput/Square.xml',
 		'TestFiles/Chapter10/SquareOutput/SquareGame.xml']
-square_out_t = \
+square_10_out_t = \
 	['TestFiles/Chapter10/SquareOutput/MainT.xml',
 		'TestFiles/Chapter10/SquareOutput/SquareT.xml',
 		'TestFiles/Chapter10/SquareOutput/SquareGameT.xml']
-SQUARE_10 = [square_in, square_out_xml, square_out_t]
+square_10_out_vm = \
+	['TestFiles/Chapter10/SquareOutput/Main.vm',
+		'TestFiles/Chapter10/SquareOutput/Square.vm',
+		'TestFiles/Chapter10/SquareOutput/SquareGame.vm']
+SQUARE_10 = [square_10_in, square_10_out_xml, square_10_out_t, square_10_out_vm]
+
 
 #########################################
 # Chapter 11 Files ######################
@@ -125,20 +137,20 @@ seven_out_vm = \
 SEVEN = [seven_in, seven_out_xml, seven_out_t, seven_out_vm]
 
 # Square
-square_in = \
+square_11_in = \
 	['TestFiles/Chapter11/Square/Main.jack',
 		'TestFiles/Chapter11/Square/Square.jack',
 		'TestFiles/Chapter11/Square/SquareGame.jack']
-square_out_xml = \
+square_11_out_xml = \
 	['TestFiles/Chapter11/Square/Main.xml',
 		'TestFiles/Chapter11/Square/Square.xml',
 		'TestFiles/Chapter11/Square/SquareGame.xml']
-square_out_t = \
+square_11_out_t = \
 	['TestFiles/Chapter11/Square/MainT.xml',
 		'TestFiles/Chapter11/Square/SquareT.xml',
 		'TestFiles/Chapter11/Square/SquareGameT.xml']
-square_out_vm = \
+square_11_out_vm = \
 	['TestFiles/Chapter11/Square/Main.vm',
 		'TestFiles/Chapter11/Square/Square.vm',
 		'TestFiles/Chapter11/Square/SquareGame.vm']
-SQUARE_11 = [square_in, square_out_xml, square_out_t, square_out_vm]
+SQUARE_11 = [square_11_in, square_11_out_xml, square_11_out_t, square_11_out_vm]
